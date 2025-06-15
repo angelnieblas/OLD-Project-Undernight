e7e5a31715b4df79495cf8cda97cea61917bbe8b
angelnieblas <starvtuber2@hotmail.com>
2025-06-14 21:31:13 -0700

    Meow

---
+++ assets/data/discord.hx
@@ -0,0 +1,50 @@
+import funkin.backend.utils.DiscordUtil;
+
+function onGameOver() {
+    DiscordUtil.changePresence('Nope.', null);
+}
+
+function onDiscordPresenceUpdate(e) {
+    var data = e.presence;
+
+    if(data.button1Label == null)
+        data.button1Label = "Project Undernight Download";
+    if(data.button1Url == null)
+        data.button1Url = "https://gamebanana.com/wips/91762";
+}
+
+function onPlayStateUpdate() {
+    DiscordUtil.changeSongPresence(
+        "Nope.",
+        null,
+        PlayState.instance.inst,
+        PlayState.instance.getIconRPC()
+    );
+}
+
+function onMenuLoaded(name:String) {
+    // Name is either "Main Menu", "Freeplay", "Title Screen", "Options Menu", "Credits Menu", "Beta Warning", "Update Available Screen", "Update Screen"
+    DiscordUtil.changePresenceSince("Nope.", null);
+}
+
+function onEditorTreeLoaded(name:String) {
+    switch(name) {
+        case "Character Editor":
+            DiscordUtil.changePresenceSince("Nope.", null);
+        case "Chart Editor":
+            DiscordUtil.changePresenceSince("Nope.", null);
+        case "Stage Editor":
+            DiscordUtil.changePresenceSince("Nope.", null);
+    }
+}
+
+function onEditorLoaded(name:String, editingThing:String) {
+    switch(name) {
+        case "Character Editor":
+            DiscordUtil.changePresenceSince("Nope.", null);
+        case "Chart Editor":
+            DiscordUtil.changePresenceSince("Nope.", null);
+        case "Stage Editor":
+            DiscordUtil.changePresenceSince("Nope.", null);
+    }
+}
